36bedf63db37fbdf8033bf74b0a3fcaf6e480ea4
anastasia-starostina <anastasia.st.stern@gmail.com>
2022-05-17 15:56:29 +0100

    Books API

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+/*PLAN
+- Find out what type of modules we are using (CommonJs) ✅
+- Export data from the book-data.js ✅
+- Import data into the app.js and put into a variable ✅
+- Set up app.get ✅
+- Have the path be '/books ✅
+- Turn data into an object 
+- Send res.json 
+
+*/
+let booksData = require('./books-data.js');
+const books = booksData.books;
+console.log(books)
 const express = require("express");
 const app = express();
 const PORT = 3000;
 
 app.get("/", function (req, res) {
   res.json({ message: "Hello from the root path!" });
+});
+
+app.get("/books", function (req, res) {
+  const responseObject = {success: true, data: books}
+  res.json(responseObject)
 });
 
 app.listen(PORT, function () {
